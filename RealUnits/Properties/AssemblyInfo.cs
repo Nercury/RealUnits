dc336926206e2c7257dffa1172a09706132d975f
Nerijus Arlauskas <nercury@gmail.com>
2010-07-11 17:18:53 +0300

    Added propper assembly information.

--- RealUnits/Properties/AssemblyInfo.cs
+++ RealUnits/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OOModel")]
-[assembly: AssemblyDescription("Office Object Model")]
+[assembly: AssemblyTitle("RealUnits")]
+[assembly: AssemblyDescription("RealUnits distance conversion library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Open Expert Office")]
-[assembly: AssemblyCopyright("Copyright © Nerijus Arlauskas 2009")]
+[assembly: AssemblyProduct("RealUnits")]
+[assembly: AssemblyCopyright("Copyright © 2009-2010 Nerijus Arlauskas and contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
